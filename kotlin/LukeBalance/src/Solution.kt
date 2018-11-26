3f2ce1118dac97a9c7c7ec6f2ecd7f158c7b64c6
chrislucas <christoffer.luccas@gmail.com>
2018-11-26 18:25:15 -0200

    estudos

--- kotlin/LukeBalance/src/Solution.kt
+++ kotlin/LukeBalance/src/Solution.kt
@@ -9,23 +9,24 @@ fun luckBalance(k: Int, contests: Array<Array<Int>>): Int {
 
     return acc
 }
+/**
+3 2
+5 1
+1 1
+4 0
+ * */
 
 fun main(args: Array<String>) {
     val data = readLine()!!.split(" ").map(String::toInt).toTypedArray()
     val n = data[0]
-    val k = data[0]
-
+    val k = data[1]
     val contest = Array(n) { it -> Array(2) {0}}
-
     for (i in 0 until  n) {
         contest[i] = readLine()!!.split(" ").map(String::toInt).toTypedArray()
     }
-
     val comp = Comparator<Array<Int>> {
-        p, q -> p[0].compareTo(q[1])
+        p, q -> q[1].compareTo(p[1])
     }
-
     contest.sortWith(comp)
-
     println(luckBalance(k, contest))
 }
