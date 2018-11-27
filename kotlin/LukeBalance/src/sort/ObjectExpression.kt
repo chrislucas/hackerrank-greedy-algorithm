fe7fab40c8531d894db07035c0bc7a25122e4270
Christoffer Lucas <christoffer.luccas@gmail.com>
2018-11-27 04:09:54 -0200

    estudos sobre comparator e object expressions

---
+++ kotlin/LukeBalance/src/sort/ObjectExpression.kt
@@ -0,0 +1,11 @@
+package sort
+
+/**
+ * https://www.programiz.com/kotlin-programming/object-singleton
+ * https://www.programiz.com/kotlin-programming/object-singleton
+ * */
+
+
+fun main(args: Array<String>) {
+
+}
